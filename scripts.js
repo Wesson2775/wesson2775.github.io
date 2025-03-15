d2453bc646ab5be0767b4160763cc4bd989d8f65
Wesson2775 <118877138+Wesson2775@users.noreply.github.com>
2025-03-15 14:28:40 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const posts = [
+    'wutai.html',
+    'xianggang.html',
+    'tianxia.html',
+  ];
+  const postsPath = './posts/';
+  const article = document.querySelector('.article');
+
+  posts.forEach(post => {
+    const filePath = postsPath + post;
+
+    fetch(filePath)
+      .then(response => response.text())
+      .then(data => {
+        const parser = new DOMParser();
+        const postDoc = parser.parseFromString(data, 'text/html');
+        const blogContent = postDoc.getElementById('blog').textContent;
+        const dateContent = postDoc.getElementById('date').textContent;
+
+        const newP = document.createElement('p');
+        const newA = document.createElement('a');
+        newA.id = 'head';
+        newA.href = filePath;
+        newA.textContent = blogContent;
+
+        const newSpan = document.createElement('span');
+        newSpan.id = 'date';
+        newSpan.textContent = dateContent;
+
+        newP.appendChild(newA);
+        newP.appendChild(document.createTextNode(' '));
+        newP.appendChild(newSpan);
+
+        article.appendChild(newP);
+      });
+  });
+});
